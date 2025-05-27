bb8ed3ae9fbddfc326dcb76e1d13c67ec8522a17
Manoj C Patil <43165241+manojcpatil@users.noreply.github.com>
2025-05-27 10:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def load_data():
 
 df = load_data()
 
-st.title("📊 Record of Diseased Farmers with Codes")
+st.title("📊 Record of Diceased Farmers with Codes")
 
 # --- Mandatory columns (always included in export) ---
 mandatory_columns = [
